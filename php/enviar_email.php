232b865bfcba0c9da8b0a7f147b88e531cf70af4
Miguel Miguez <miguelmiguezangel@gmail.com>
2024-03-18 12:19:26 -0300

    update

--- php/enviar_email.php
+++ php/enviar_email.php
@@ -5,7 +5,7 @@ $email = $_POST['email'];
 $number = $_POST['number'];
 
 // Dirección de correo electrónico a la que enviar el mensaje
-$to = "contacto@digitalrain.online";
+$to = "miguelmiguezangel@gmail.com";
 
 // Asunto del correo electrónico
 $subject = "Mensaje de contacto desde el sitio web";
@@ -21,10 +21,5 @@ $headers = "From: $email";
 // Envía el correo electrónico
 $mail_success = mail($to, $subject, $message, $headers);
 
-// Verifica si el correo electrónico se envió correctamente
-if ($mail_success) {
-    echo "<p>¡El mensaje se envió correctamente!</p>";
-} else {
-    echo "<p>¡Hubo un error al enviar el mensaje! Por favor, inténtalo de nuevo más tarde.</p>";
-}
+header('Location:index.html');
 ?>
